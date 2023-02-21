2c83d9c6b33613b96bda600f4f05514e46459fa1
[abdallah_marwad] <[abdallahshehata311as@gmail.com]>
2023-02-21 17:04:17 +0200

    forth Commit

--- app/src/main/java/com/example/gitstudying/MainActivity.kt
+++ app/src/main/java/com/example/gitstudying/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("Second Commit")
-        hello abdo
+        print("forth Commit")
     }
 }
