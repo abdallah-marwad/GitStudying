f1190c9bbbd0dc56a5b3893557489b4443c8f492
[abdallah_marwad] <[abdallahshehata311as@gmail.com]>
2023-02-22 14:32:05 +0200

    5 Commit

--- app/src/main/java/com/example/gitstudying/MainActivity.kt
+++ app/src/main/java/com/example/gitstudying/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("Second Commit")
-        hello abdo
+        print("5 Commit")
     }
 }
