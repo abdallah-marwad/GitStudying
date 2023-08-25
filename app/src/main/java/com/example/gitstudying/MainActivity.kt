599fc20fe906d7d8a82a029588f2e510295b668c
[abdallah_marwad] <[bodmakkalg@gmail.com]>
2023-08-25 12:08:44 +0300

    main new branch 1 commit

--- app/src/main/java/com/example/gitstudying/MainActivity.kt
+++ app/src/main/java/com/example/gitstudying/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         print("Main branch")
         print("Main new branch")
+        print("Main new branch")
 
     }
 }
