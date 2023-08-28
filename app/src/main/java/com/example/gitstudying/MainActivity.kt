1e55500bc1d4812e0e3b1f86b73abc229e9a42b6
[abdallah_marwad] <[bodmakkalg@gmail.com]>
2023-08-28 06:18:29 +0300

    commit old

--- app/src/main/java/com/example/gitstudying/MainActivity.kt
+++ app/src/main/java/com/example/gitstudying/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("Main branch")
-        print("Main branch")
+        print("commit Main branch old")
 
     }
 }
